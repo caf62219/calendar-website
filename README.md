a51dcd5c211eefb5e929b7f4c0f465dcc6150bab
Chelsea Wagner <1chelsea.fischer1@gmail.com>
2023-07-15 16:50:48 -0700

    updates to javascript trying to get save to work

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# calendar-website
-This is a calendar website for a single day for a user.
+# Calendar Website
+
+# Description
+    -This project involved taking existing code and making a functional calendar website.
+    -The calendar website is responsive to the current date/time and changes colors accordingly.
+    -When a user inputs an event in an hour the event is stored and remains on the page.
+    
+    
+# Installation
+
+    Follow the following URL to the website:
+    
+# Usage
+<img src="" alt="screenshot of calendar website" width="600px" />
+   
+    - If you type in one of the boxes the content is saved
+
+# Credits
+    Donnie Rawlings: https://github.com/drawlin22/
+    Received assistance from Tutor Neil Dhand.
+    Received assistance from askBCS learning assistant Jose on changing the colors
+
+
+# License
+    N/A
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,21 +1,26 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function save () {
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-// var description = $("#input-9")
-  
 
-$(".btn").on("click", function(){
-    localStorage.setItem("event", )
+  $(function saveEvent(event) {
+  var parentId = this.parentElement.id;
+  var event= this.previousElementSibling.value;
+  localStorage.setItem(parentId, JSON.stringify(event))
+})
+
+
+$(".saveBtn").on("click", function(){
+    localStorage.setItem(parentId, saveEvent)
 
   })
-})
+
 
 
 
